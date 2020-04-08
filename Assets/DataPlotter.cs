39cf6f67e8bb15790962c4edc27cf754af76be06
Kun-Neng <kunneng.hung@gmail.com>
2020-04-08 14:43:27 +0800

    create functions for reading file and ploting

---
+++ Assets/DataPlotter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataPlotter : MonoBehaviour {
+	public string inputfile = "Assets/iris-2lg34dn.csv"; // "D:\\iris-2lg34dn.csv";
+	public GameObject PointPrefab;
+
+	// private List<Dictionary<string, object>> pointList;
+
+	// Use this for initialization
+	void Start () {
+		// pointList = CSVReader.Read(inputfile);
+		// Debug.Log(pointList);
+		List<float[]> filedata = HandleTextFile.ReadString(inputfile);
+
+		// Debug.Log(filedata);
+		filedata.ForEach (delegate(float[] obj) {
+			Instantiate (PointPrefab, new Vector3 (obj [0], obj [1], obj [2]), Quaternion.identity);
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/HandleTextFile.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HandleTextFile {
+	public static List<float[]> ReadString(string file) {
+		// string path = "Assets/Resources/test.txt";
+		//Read the text from directly from the test.txt file
+		StreamReader filereader = new StreamReader(file);
+		string filedata = filereader.ReadToEnd();
+
+		List<float[]> pointList = new List<float[]>();
+		char seperator = ',';
+		using (StringReader reader = new StringReader(filedata)) {
+			string line;
+			while ((line = reader.ReadLine()) != null) {
+				// Debug.Log(line);
+				string[] pointtoken = line.Split(seperator);
+				float x = Convert.ToSingle(pointtoken [0]);
+				float y = Convert.ToSingle(pointtoken [1]);
+				float z = Convert.ToSingle(pointtoken [2]);
+				float[] point = new float[]{x, y, z};
+				pointList.Add(point);
+				//Debug.Log(pointtoken[0], pointtoken[1], pointtoken[2]);
+			}
+
+			reader.Close();
+		}
+
+		return pointList;
+	}
+}
